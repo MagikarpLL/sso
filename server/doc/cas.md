10100460f936f9d04b1551210f2f282ee5e33c62
MagikarpLL <magikarpllxd@gmail.com>
2021-01-28 22:24:33 +0800

    init

---
+++ server/doc/cas.md
@@ -0,0 +1,34 @@
+# CAS6.3版本改造记录
+
+## 改造目标
+- 由原来的war-ovelay插件的war包部署方式改为maven引入依赖，jar包启动
+- 自定义并改造cas的webflow流程
+- 自定义前端的展示页面，可能完全更换引擎，不使用自带的thymeleaf模板
+- 支持rest接口登录与oauth2.0接口认证
+
+## 改造记录
+### war包改造
+已基本修改完成  
+
+改造方案如下：  
+将原生的6.3war包拉下来，然后分析其引入的依赖，然后另外重新搭建一个maven springboot jar工程，并将原来war包的依赖逐渐引入
+### Springboot初始化方面
+正在进行中
+
+目前改造方案如下:  
+- 将原生的cas-server-webapp-init jar包中的CasWebApplication拷贝出来，并以springboot方式启动即可
+
+遇到的问题如下:
+- cas自带的日志与springboot框架的日志冲突，因此需要屏蔽掉springboot原生的日志框架
+### Webflow 方面
+正在进行中
+
+目前改造方案如下:
+- 继承cas-server-code-webflow-api 包中的AbstractCasWebflowConfigurer并自定义它，即可实现自己的webflow流程
+
+遇到的问题如下:
+- loing-webflow.xml并不能移除，且原生的webflow配置需要屏蔽掉.他们现阶段的做法是，放两个webflow的xml文件到资源目录，然后通过java代码向里面添加action等流程
+
+### 前端展示页面
+
+### REST接口方面
--- server/src/main/java/cn/magikarpll/sso/server/Application.java
+++ server/src/main/java/cn/magikarpll/sso/server/Application.java
@@ -1,16 +1,74 @@
 package cn.magikarpll.sso.server;
 
+import lombok.Generated;
+import org.apereo.cas.CasEmbeddedContainerUtils;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.web.CasWebApplication;
+import org.apereo.cas.web.CasWebApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.actuate.autoconfigure.metrics.KafkaMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+import java.time.Instant;
+
+@EnableDiscoveryClient
+@SpringBootApplication(
+        exclude = {HibernateJpaAutoConfiguration.class, JerseyAutoConfiguration.class, GroovyTemplateAutoConfiguration.class, GsonAutoConfiguration.class, JmxAutoConfiguration.class, DataSourceAutoConfiguration.class, RedisAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class, KafkaAutoConfiguration.class, KafkaMetricsAutoConfiguration.class, CassandraAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class},
+        proxyBeanMethods = false
+)
+@EnableConfigurationProperties({CasConfigurationProperties.class})
+@EnableAsync
+@EnableAspectJAutoProxy(
+        proxyTargetClass = true
+)
+@EnableTransactionManagement(
+        proxyTargetClass = true
+)
 @EnableScheduling
 public class Application {
-    public static void main( String[] args )
-    {
-        SpringApplication.run(Application.class);
+
+    @Generated
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(final String[] args) {
+        Banner banner = CasEmbeddedContainerUtils.getCasBannerInstance();
+        (new SpringApplicationBuilder(new Class[]{CasWebApplication.class})).banner(banner).web(WebApplicationType.SERVLET).logStartupInfo(true).contextClass(CasWebApplicationContext.class).run(args);
+    }
+
+    @EventListener
+    public void handleApplicationReadyEvent(final ApplicationReadyEvent event) {
+//        AsciiArtUtils.printAsciiArtReady(LOGGER, "");
+//        LOGGER.info("Ready to process requests @ [{}]", DateTimeUtils.zonedDateTimeOf(Instant.ofEpochMilli(event.getTimestamp())));
+    }
+
+    @Generated
+    public Application() {
     }
 
 }
