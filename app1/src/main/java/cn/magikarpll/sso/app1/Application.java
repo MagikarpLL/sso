528d02477905cd4f315ef85f4c36b6af45b77dd6
MagikarpLL <magikarpllxd@gmail.com>
2021-02-03 14:30:01 +0800

    用于测试登录的应用

---
+++ app1/src/main/java/cn/magikarpll/sso/app1/Application.java
@@ -0,0 +1,13 @@
+package cn.magikarpll.sso.app1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
---
+++ app1/src/main/java/cn/magikarpll/sso/app1/controller/TestController.java
@@ -0,0 +1,16 @@
+package cn.magikarpll.sso.app1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/get")
+    public String get(){
+        return "get";
+    }
+
+}
